b59bff1ab512a15df6fbfe5f0a6f6c4b66407741
Ashish  Tiwari <115910726+0xashishtiwari@users.noreply.github.com>
2024-11-16 01:49:34 +0000

    let & var , const

--- Basic/test.js
+++ Basic/test.js
@@ -1,1 +1,10 @@
-console.log("ashish")
+const accountId = 144553
+let accountEmail = "ashish@google.com"
+var accountPassword = "12414"
+accountCity  = "Rewa"
+let accountState
+// accountId = 2
+console.log(accountId);
+accountEmail = "akt@gmail.com"
+
+console.table([accountId , accountPassword , accountCity , accountEmail , accountState])
